84794786d8fea6cb3b1219a40417a29360fcc054
592402095 <592402095@qq.com>
2019-11-26 19:29:18 +0800

    321

---
+++ tests/test_tokenizer.cpp
@@ -0,0 +1,23 @@
+#include "catch2/catch.hpp"
+#include "tokenizer/tokenizer.h"
+#include "fmt/core.h"
+
+#include <sstream>
+#include <vector>
+
+// 下面是示例如何书写测试用例
+TEST_CASE("Test hello world.") {
+	
+	std::string input =
+		"1";
+	std::stringstream ss;
+	ss.str(input);
+	miniplc0::Tokenizer tkz(ss);
+	std::vector<miniplc0::Token> output = {};
+	auto result = tkz.AllTokens();
+	if (result.second.has_value()) {
+		FAIL();
+	}
+	REQUIRE( (result.first == output) );
+	
+}
